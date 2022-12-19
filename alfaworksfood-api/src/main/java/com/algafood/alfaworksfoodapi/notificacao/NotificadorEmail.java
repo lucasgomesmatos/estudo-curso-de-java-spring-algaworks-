eb33849b1b503d8de7bc2d2acbcbb605eba2402e
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-12-19 06:16:36 -0300

    Configurando servidor smtp e port customizados

--- alfaworksfood-api/src/main/java/com/algafood/alfaworksfoodapi/notificacao/NotificadorEmail.java
+++ alfaworksfood-api/src/main/java/com/algafood/alfaworksfoodapi/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.algafood.alfaworksfoodapi.notificacao;
 
 import com.algafood.alfaworksfoodapi.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +10,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+    @Value("${notificador.email.port-servidor}")
+    private Integer porta;
+
     public NotificadorEmail() {
         System.out.println("Notificador real");
     }
 
     @Override
-    public void notificar( Cliente cliente, String mensagem) {
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Porta: " + porta);
+
         System.out.printf("Notificando %s através do email %s : %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem
         );
