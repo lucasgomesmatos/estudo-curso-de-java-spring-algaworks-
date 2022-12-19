0bc8ed4c9933d584864d39d7bf0bbbb8205a80c8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-12-19 06:28:27 -0300

    Configurando proptiedades customizadas

--- alfaworksfood-api/src/main/java/com/algafood/alfaworksfoodapi/notificacao/NotificadorEmail.java
+++ alfaworksfood-api/src/main/java/com/algafood/alfaworksfoodapi/notificacao/NotificadorEmail.java
@@ -1,7 +1,7 @@
 package com.algafood.alfaworksfoodapi.notificacao;
 
 import com.algafood.alfaworksfoodapi.modelo.Cliente;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
-    @Value("${notificador.email.host-servidor}")
-    private String host;
-    @Value("${notificador.email.port-servidor}")
-    private Integer porta;
+//    @Value("${notificador.email.host-servidor}")
+//    private String host;
+//    @Value("${notificador.email.port-servidor}")
+//    private Integer porta;
+
+    @Autowired
+    private NotificadorProperties notificadorProperties;
 
     public NotificadorEmail() {
         System.out.println("Notificador real");
@@ -21,8 +24,8 @@ public class NotificadorEmail implements Notificador {
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.println("Host: " + host);
-        System.out.println("Porta: " + porta);
+        System.out.println("Host: " + notificadorProperties.getHostServidor());
+        System.out.println("Porta: " + notificadorProperties.getPortServidor());
 
         System.out.printf("Notificando %s através do email %s : %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem
