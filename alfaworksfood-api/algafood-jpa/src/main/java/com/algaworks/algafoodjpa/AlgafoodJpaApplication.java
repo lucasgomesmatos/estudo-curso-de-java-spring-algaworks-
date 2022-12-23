069a546d476c294d184a2e407f4ae805fbf025a8
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2022-12-22 22:12:23 -0300

    jpa

---
+++ alfaworksfood-api/algafood-jpa/src/main/java/com/algaworks/algafoodjpa/AlgafoodJpaApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgafoodJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgafoodJpaApplication.class, args);
+    }
+
+}
---
+++ alfaworksfood-api/algafood-jpa/src/test/java/com/algaworks/algafoodjpa/AlgafoodJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafoodjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
