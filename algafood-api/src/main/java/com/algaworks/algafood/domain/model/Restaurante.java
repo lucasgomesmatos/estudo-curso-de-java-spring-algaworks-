a9d927be377a1413dd510c8d0dc8f2db9657fe73
Lucas Gomes Matos <lgomes.lg99@gmail.com>
2023-01-08 23:31:35 -0300

    java

--- algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
+++ algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -7,11 +7,7 @@ import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
@@ -30,6 +26,7 @@ public class Restaurante {
 	@Column(name = "taxa_frete")
 	private BigDecimal taxaFrete;
 
-
+	@ManyToOne
+	private Cozinha cozinha;
 	
 }
--- algafood-api/src/main/resources/import.sql
+++ algafood-api/src/main/resources/import.sql
@@ -1,2 +1,8 @@
-insert into cozinha (nome) values ('Tailandesa');
-insert into cozinha (nome) values ('Indiana');
+insert into cozinha (id, nome) values (1 , 'Tailandesa');
+insert into cozinha (id, nome) values (2, 'Indiana');
+insert into cozinha (id, nome) values (3, 'Brasil');
+
+
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("taik", 10, 1)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("room", 10, 2)
+insert into restaurante (nome, taxa_frete, cozinha_id) values ("arroz", 10, 3)
