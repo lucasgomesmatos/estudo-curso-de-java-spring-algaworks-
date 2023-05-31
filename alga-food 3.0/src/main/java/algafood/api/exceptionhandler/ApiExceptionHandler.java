26c340490f48598bd392d85732ace2416698c7cd
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-05-30 22:26:39 -0300

    exception handler golbal

--- alga-food 3.0/src/main/java/algafood/api/exceptionhandler/ApiExceptionHandler.java
+++ alga-food 3.0/src/main/java/algafood/api/exceptionhandler/ApiExceptionHandler.java
@@ -3,17 +3,16 @@ package algafood.api.exceptionhandler;
 import algafood.domain.exception.EntidadeEmUsoException;
 import algafood.domain.exception.EntidadeNaoEncontradaException;
 import algafood.domain.exception.NegocioException;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
 
 @ControllerAdvice
-public class ApiExceptionHandler {
+public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(EntidadeNaoEncontradaException.class)
     public ResponseEntity<?> tratarEstadoNaoEncontadoException(EntidadeNaoEncontradaException e) {
@@ -43,21 +42,7 @@ public class ApiExceptionHandler {
         return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
     }
 
-    @ExceptionHandler(DataIntegrityViolationException.class)
-    public  ResponseEntity<?> tratarDataIntegrityViolationException(DataIntegrityViolationException e) {
-        var erro = ApiExceptionError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-        return ResponseEntity.status(HttpStatus.CONFLICT).body(erro);
-    }
 
-    @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
-    public  ResponseEntity<?> tratarHttpMediaTypeNotSupportedException() {
-        var erro = ApiExceptionError.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem("Tipo de mídia não é aceito")
-                .build();
-        return ResponseEntity.status(HttpStatus.UNSUPPORTED_MEDIA_TYPE).body(erro);
-    }
+
+
 }
