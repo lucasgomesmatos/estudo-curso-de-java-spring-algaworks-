3a16af27e6a4c07d51cdd6960f332a2f9948e35d
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-06-05 23:03:51 -0300

    exception parametro url invalido

--- alga-food 3.0/src/main/java/algafood/api/exceptionhandler/ProblemType.java
+++ alga-food 3.0/src/main/java/algafood/api/exceptionhandler/ProblemType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ProblemType {
 
+    PARAMETRO_INVALIDO("Parâmetro inválido", "/parametro-invalido"),
     MENSAGEM_INCOMPREENSIVEL("Mensagem incompreensível", "/mensagem-imcompreensível"),
     ENTIDADE_NAO_ENCONTADA("Entidade não encontrada", "/entidade-nao-encontada"),
     ENTIDADE_EM_USO("Entidade em uso", "/entidade-em-uso"),
