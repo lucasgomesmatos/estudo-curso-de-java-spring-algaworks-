fb253e600014aa735b83c05fa1e7a405fd859291
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-11 23:18:32 -0300

    testes

--- alga-food 3.0/src/test/java/algafood/ApplicationTests.java
+++ alga-food 3.0/src/test/java/algafood/ApplicationTests.java
@@ -3,11 +3,14 @@ package algafood;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 @SpringBootTest
 class ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertFalse(false);
 	}
 
 }
