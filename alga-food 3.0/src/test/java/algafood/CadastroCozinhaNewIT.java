d8bd8b9caa4ee5f22c3f4615b1d7593ddb4462a5
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-18 23:09:25 -0300

    before teste

--- alga-food 3.0/src/test/java/algafood/CadastroCozinhaNewIT.java
+++ alga-food 3.0/src/test/java/algafood/CadastroCozinhaNewIT.java
@@ -1,7 +1,9 @@
 package algafood;
 
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,12 +22,17 @@ public class CadastroCozinhaNewIT {
     @LocalServerPort
     private int port;
 
+    @Before
+    public void setUp() {
+        enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.port= port;
+        RestAssured.basePath = "api/cozinhas";
+    }
+
     @Test
     public void deveRetornarStatus200QuandoConsultarCozinhas() {
-        enableLoggingOfRequestAndResponseIfValidationFails();
 
         given()
-                .basePath("api/cozinhas")
                 .port(port)
                 .accept(ContentType.JSON)
                 .when()
@@ -40,7 +47,6 @@ public class CadastroCozinhaNewIT {
         enableLoggingOfRequestAndResponseIfValidationFails();
 
         given()
-                .basePath("api/cozinhas")
                 .port(port)
                 .accept(ContentType.JSON)
                 .when()
