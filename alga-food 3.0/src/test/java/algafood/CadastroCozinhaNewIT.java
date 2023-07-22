156ccaa8560ac41ecc1449b77000c7f1683d4fbd
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-22 20:06:35 -0300

    refatoracao dos testes

--- alga-food 3.0/src/test/java/algafood/CadastroCozinhaNewIT.java
+++ alga-food 3.0/src/test/java/algafood/CadastroCozinhaNewIT.java
@@ -1,9 +1,13 @@
 package algafood;
 
+import algafood.common.Serializer;
+import algafood.domain.models.Cozinha;
+import algafood.domain.repositories.CozinhaRepository;
+import algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.flywaydb.core.Flyway;
 import org.hamcrest.Matchers;
+import org.jboss.logging.Logger;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,21 +15,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static algafood.common.GetContentFromResource.getContentFromResource;
 import static io.restassured.RestAssured.enableLoggingOfRequestAndResponseIfValidationFails;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaNewIT {
 
     @LocalServerPort
     private int port;
 
     @Autowired
-    private Flyway flyway;
+    DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+    static Logger logger = Logger.getLogger(Serializer.class);
+
+    private Cozinha cozinhaIndiana;
+    private int quantidadeCozinhasCadastradas;
+    private static final int COZINHA_ID_INEXISTENTE = 100;
+
 
     @Before
     public void setUp() {
@@ -33,7 +51,8 @@ public class CadastroCozinhaNewIT {
         RestAssured.port = port;
         RestAssured.basePath = "api/cozinhas";
 
-        flyway.migrate();
+        databaseCleaner.clearTables();
+        prepararDados();
     }
 
     @Test
@@ -50,7 +69,7 @@ public class CadastroCozinhaNewIT {
     }
 
     @Test
-    public void deveRetornar4CozinhasQuandoConsultarCozinhas() {
+    public void deveRetornar2CozinhasQuandoConsultarCozinhas() {
         enableLoggingOfRequestAndResponseIfValidationFails();
 
         given()
@@ -59,19 +78,58 @@ public class CadastroCozinhaNewIT {
                 .when()
                 .get()
                 .then()
-                .body("", Matchers.hasSize(4))
-                .body("nome", Matchers.hasItems("Indiana", "Brasil"))
+                .body("", Matchers.hasSize(quantidadeCozinhasCadastradas))
+                .body("nome", Matchers.hasItems("Tailandesa", "Indiana"))
         ;
     }
 
     @Test
     public void deveRetornarStatus201QuandoCadastrarCozinhas() {
+
+        var nomeCozinha = getContentFromResource("/mockJson/cozinha-chinesa.json");
+
         given()
-                .body("{\"nome\": \"Chinesa\" }")
+                .body(nomeCozinha)
                 .contentType(ContentType.JSON)
                 .accept(ContentType.JSON)
                 .when()
                 .post()
                 .then().statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    public void deveRetornarRespostaEStatusCorretosQuandoConsultarCozinhaExistente() {
+
+        given()
+                .pathParam("cozinhaId", quantidadeCozinhasCadastradas)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/{cozinhaId}")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .body("nome", equalTo(cozinhaIndiana.getNome()));
+    }
+
+    @Test
+    public void deveRetornarRespostaEStatus404CorretosQuandoConsultarCozinhaInexistente() {
+        given()
+                .pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
+                .accept(ContentType.JSON)
+                .when()
+                .get("/{cozinhaId}")
+                .then()
+                .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha1);
+
+        cozinhaIndiana = new Cozinha();
+        cozinhaIndiana.setNome("Indiana");
+        cozinhaRepository.save(cozinhaIndiana);
+
+        quantidadeCozinhasCadastradas = (int) cozinhaRepository.count();
     }
 }
