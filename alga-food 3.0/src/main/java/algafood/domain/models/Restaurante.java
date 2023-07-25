391893de08d2dd3476a159814ff2b89d71e24973
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-07-24 22:44:55 -0300

    data em timezone utc

--- alga-food 3.0/src/main/java/algafood/domain/models/Restaurante.java
+++ alga-food 3.0/src/main/java/algafood/domain/models/Restaurante.java
@@ -9,7 +9,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -32,15 +32,15 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
-    @JsonIgnore
+    //@JsonIgnore
     @CreationTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataCadastro;
+    private OffsetDateTime dataCadastro;
 
-    @JsonIgnore
+    //@JsonIgnore
     @UpdateTimestamp
     @Column(nullable = false, columnDefinition = "datetime")
-    private LocalDateTime dataAtualizacao;
+    private OffsetDateTime dataAtualizacao;
 
     // @JsonIgnore
     // @JsonIgnoreProperties("hibernateLazyInitializer")
