a2bdb59e0182403f28ed6dcb04e10dfa6a14b71f
Lucas Gomes Matos <lucasgomesmatosdev@gmail.com>
2023-11-15 23:40:39 -0300

    data

--- alga-food 3.0/src/main/java/algafood/Application.java
+++ alga-food 3.0/src/main/java/algafood/Application.java
@@ -13,7 +13,6 @@ public class Application {
 
 	public static void main(String[] args) {
 		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
-
 		SpringApplication.run(Application.class, args);
 	}
 
